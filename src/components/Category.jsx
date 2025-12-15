809aa31005f133e265623cd48a1a974d66dadae9
Pannnomaly <supawith007@gmail.com>
2025-12-15 21:08:47 +0700

    feat: add category section

---
+++ src/components/Category.jsx
@@ -0,0 +1,55 @@
+export default function Category ()
+{
+    return (
+        <div id="category" className="bg-white px-10 py-15">
+            <a href="#" className="flex flex-col justify-center items-center pb-5 hover:text-(--main-dark) transition duration-300 ease-in-out">
+                <div className="bg-(--main-light) rounded-[50%] w-[150px] h-[150px] p-5 hover:opacity-[0.8] transition duration-300 ease-in-out">
+                    <img src="c1.png" alt="Annual Flower" className="w-full h-full object-contain object-center" />
+                </div>
+                <div>
+                    <p className="text-2xl font-semibold mt-3">Annual Flower</p>
+                </div>
+            </a>
+            <a href="#" className="flex flex-col justify-center items-center pb-5 hover:text-(--main-dark) transition duration-300 ease-in-out">
+                <div className="bg-(--main-light) rounded-[50%] w-[150px] h-[150px] p-5 hover:opacity-[0.8] transition duration-300 ease-in-out">
+                    <img src="c2.png" alt="Perennial Flower" className="w-full h-full object-contain object-center" />
+                </div>
+                <div>
+                    <p className="text-2xl font-semibold mt-3">Perennial Flower</p>
+                </div>
+            </a>
+            <a href="#" className="flex flex-col justify-center items-center pb-5 hover:text-(--main-dark) transition duration-300 ease-in-out">
+                <div className="bg-(--main-light) rounded-[50%] w-[150px] h-[150px] p-5 hover:opacity-[0.8] transition duration-300 ease-in-out">
+                    <img src="c3.png" alt="Biennial Flower" className="w-full h-full object-contain object-center" />
+                </div>
+                <div>
+                    <p className="text-2xl font-semibold mt-3">Biennial Flower</p>
+                </div>
+            </a>
+            <a href="#" className="flex flex-col justify-center items-center pb-5 hover:text-(--main-dark) transition duration-300 ease-in-out">
+                <div className="bg-(--main-light) rounded-[50%] w-[150px] h-[150px] p-5 hover:opacity-[0.8] transition duration-300 ease-in-out">
+                    <img src="c4.png" alt="Rose Flower" className="w-full h-full object-contain object-center" />
+                </div>
+                <div>
+                    <p className="text-2xl font-semibold mt-3">Rose Flower</p>
+                </div>
+            </a>
+            <a href="#" className="flex flex-col justify-center items-center pb-5 hover:text-(--main-dark) transition duration-300 ease-in-out">
+                <div className="bg-(--main-light) rounded-[50%] w-[150px] h-[150px] p-5 hover:opacity-[0.8] transition duration-300 ease-in-out">
+                    <img src="c5.png" alt="SunFlower" className="w-full h-full object-contain object-center" />
+                </div>
+                <div>
+                    <p className="text-2xl font-semibold mt-3">Sunflower</p>
+                </div>
+            </a>
+            <a href="#" className="flex flex-col justify-center items-center pb-5 hover:text-(--main-dark) transition duration-300 ease-in-out">
+                <div className="bg-(--main-light) rounded-[50%] w-[150px] h-[150px] p-5 hover:opacity-[0.8] transition duration-300 ease-in-out">
+                    <img src="c6.png" alt="Hydrangea" className="w-full h-full object-contain object-center" />
+                </div>
+                <div>
+                    <p className="text-2xl font-semibold mt-3">Hydrangea</p>
+                </div>
+            </a>
+        </div>
+    );
+}
